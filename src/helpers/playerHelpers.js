1643e11f606aa737de989d3efb3fbcefef0ee08b
Taylor Noj <taylornoj@gmail.com>
2021-12-04 22:58:58 -0500

    populated helper functions

--- src/helpers/playerHelpers.js
+++ src/helpers/playerHelpers.js
@@ -1,7 +1,26 @@
-export const preparePlayerData = () => {
+  // input: playerData
+export const preparePlayerData = (playerData) => {
+  return Object.values(playerData);
+}
+ // output: [{player}, {player}]
+
+
+ // input: playerDataArray, matchData
+export const addWinsToPlayers = (playerDataArray, matchData) => {
+  return playerDataArray.map(player => {
+    // Calculating the number of wins in the matchData
+    const currentWins = matchData.reduce((accumulator, match) => {
+      // Adds a win if the gamerTag matches
+      if (match.winner === player.gamerTag) {
+        return accumulator += 1;
+      } else {
+        return accumulator;
+      }
+    }, 0);
+    // Assigns the value to the wins key
+    player.wins=currentWins;
+    return player;
+  })
 
 }
-
-export const addWinsToPlayers = () => {
-
-}
+ // output: [{player}, {player}] (each player having a win key and a numerical value)
