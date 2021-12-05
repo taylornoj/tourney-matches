f06adb9c1e14661a88fccbaabefbf9008524ec2a
Taylor Noj <taylornoj@gmail.com>
2021-12-04 19:59:37 -0500

    add starter HTML in components

---
+++ src/components/MatchList.js
@@ -0,0 +1,4 @@
+<section className="PlayerList MatchList">
+<h1>Match list</h1>
+{/* Matches will be shown here */}
+</section>
---
+++ src/components/PlayerList.js
@@ -0,0 +1,4 @@
+<section className="PlayerList">
+    <h1>Current participating players</h1>
+    {/* Players will be shown here */}
+  </section>
